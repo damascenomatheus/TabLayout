089751ab54631337064d3d29119387714cbd14a0
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-13 08:53:41 -0300

    Updating label color

--- YoutubeTutorial/View/MenuCell.swift
+++ YoutubeTutorial/View/MenuCell.swift
@@ -13,7 +13,7 @@ class MenuCell: BaseCell {
     lazy var trailLabel: UILabel = {
         let label = UILabel()
         label.lineBreakMode = .byWordWrapping
-        label.textColor = .white
+        label.textColor = #colorLiteral(red: 0.3568627451, green: 0.05490196078, blue: 0.05098039216, alpha: 1)
         label.font = UIFont.boldSystemFont(ofSize: 20)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
